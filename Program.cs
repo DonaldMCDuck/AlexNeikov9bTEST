148d14d8ea531460d0e54d31d55bc7c1be39028a
Alex Neikov <51147680+DonaldMCDuck@users.noreply.github.com>
2019-05-29 08:29:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexNeikov9bTEST
+{
+    class Program
+    {
+        static void Vuvejdane()
+        {
+            Console.WriteLine("Enter string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+        }
+        static bool IsPalindrome(string value)
+        {
+            int i = 0;
+            int j = value.Length - 1;
+            while (true)
+            {
+                if (i > j)
+                {
+                    return true;
+                }
+                char a = value[i];
+                char b = value[j];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                i++;
+                j--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            //Za kapak, predi godini grupa ABBA specheli Evroviziq.
+            Vuvejdane();
+            IsPalindrome();
+            Console.ReadKey();
+        }
+    }
+}
